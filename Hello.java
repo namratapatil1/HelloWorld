ca704d31453abffef215d9695ad284f91714ad83
Namrata Kulkarni <Namrata_Kulkarni@symantec.com>
2017-08-01 14:34:25 +0530

    added Hell java programs

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/*
+* First Java Program to say Hello Namrata
+*/
+public class Hello{
+	public static void main(String[] args){
+		for (int i=1; i<=10; i++){
+			System.out.println("Hello , Namrata .. Have a beautiful day!!!"+i);
+		}
+	}
+}
